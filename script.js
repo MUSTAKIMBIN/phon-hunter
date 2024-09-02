944bc4f8f4a39cec756000b8cb5acc499d30131f
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-09-02 09:59:45 +0600

    added initial file setup and checked api

---
+++ script.js
@@ -0,0 +1,10 @@
+const loadPhon = async () => {
+  const res = await fetch(
+    " https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data
+  console.log(phones);
+};
+
+loadPhon()
