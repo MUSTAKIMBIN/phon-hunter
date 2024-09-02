62a76d1b32b4f377cebb08caab88da26de709958
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-09-02 15:22:47 +0600

    create a function that fetch data and display all data

--- script.js
+++ script.js
@@ -3,8 +3,33 @@ const loadPhon = async () => {
     " https://openapi.programming-hero.com/api/phones?search=iphone"
   );
   const data = await res.json();
-  const phones = data.data
-  console.log(phones);
+  const phones = data.data;
+  displayPhones(phones);
 };
 
-loadPhon()
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("card", "bg-base-100", "shadow-xl");
+    phoneDiv.innerHTML = `
+      <figure class="px-10 pt-10">
+        <img
+          src="${phone.image}"
+          alt="Shoes"
+          class="rounded-xl" />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">${phone.phone_name}</h2>
+        <p>${phone.brand}</p>
+        <div class="card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    `;
+    phoneContainer.appendChild(phoneDiv);
+  });
+};
+
+loadPhon();
